8995559b6ffb3b8236df776f8d123741a30f5276
Singh <jaideep.f.singh@accenture.com>
2019-06-29 02:08:44 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './lazyModules/settings/settings.component';
+import { ProfileComponent } from './lazyModules/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SettingsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
